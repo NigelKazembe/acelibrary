06b40444a8a2cb59c4ad213e130c455155b0afcd
NigelKazembe <nigelkazembe9@gmail.com>
2022-11-07 02:23:05 +0200

    Initial commit

---
+++ acelibrary/src/main/java/com/nigelkazembe/acelibrary/OnLoadedEditorListener.java
@@ -0,0 +1,10 @@
+package com.nigelkazembe.acelibrary;
+
+/**
+ * Created by susmit on 25/1/18.
+ */
+
+public interface OnLoadedEditorListener
+{
+    void onCreate();
+}
---
+++ acelibrary/src/main/java/com/nigelkazembe/acelibrary/ResultReceivedListener.java
@@ -0,0 +1,10 @@
+package com.nigelkazembe.acelibrary;
+
+/**
+ * Created by susmit on 25/1/18.
+ */
+
+public interface ResultReceivedListener
+{
+    void onReceived(int FLAG_VALUE, String... results);
+}
